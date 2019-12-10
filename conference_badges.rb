379ab1328bf446301be598acc5aaefc923022767
Steven Kumarsingh <stevenks17@gmail.com>
2019-12-10 17:45:28 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end  
+
+def batch_badge_creator(attendees)  \
+   attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+     return "Hello, my name is #{attendees}."
+ 
+end  
