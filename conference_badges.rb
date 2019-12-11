9fb1b4bf26a52c3567aff3211ec02a36e7ff009e
Sehrish Balouch <sehrishbalouch4@gmail.com>
2019-12-11 18:15:37 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,6 @@
-# Write your code here.
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def badge_maker(name)
+  speakers.each do |name|
+    puts "Hello, my name is #{name}"
+    
+  end 
